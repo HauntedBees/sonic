2ba012e27923eac3a6d9a59fd1cc2946e6660073
Sean Finch <fench@hauntedbees.com>
2020-07-26 19:13:38 -0700

    data update

--- schema/schema.sql
+++ schema/schema.sql
@@ -1,12 +1,6 @@
---
--- Database: `sonic`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `category`
---
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 CREATE TABLE `category` (
   `id` int(11) NOT NULL,
@@ -15,37 +9,20 @@ CREATE TABLE `category` (
   `icon` varchar(45) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `categoryrelationships`
---
-
 CREATE TABLE `categoryrelationships` (
   `parent` int(11) NOT NULL,
   `child` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `entity`
---
 
 CREATE TABLE `entity` (
   `id` int(11) NOT NULL,
   `name` varchar(150) NOT NULL,
   `type` int(11) DEFAULT NULL,
   `description` varchar(1000) DEFAULT NULL,
+  `img` int(11) NOT NULL,
   `iconx` int(11) DEFAULT NULL,
   `icony` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `feedback`
---
 
 CREATE TABLE `feedback` (
   `id` int(11) NOT NULL,
@@ -57,12 +34,6 @@ CREATE TABLE `feedback` (
   `date` datetime DEFAULT NULL,
   `dismissed` tinyint(4) DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `issues`
---
 
 CREATE TABLE `issues` (
   `id` int(11) NOT NULL,
@@ -76,25 +47,13 @@ CREATE TABLE `issues` (
   `contentwarning` varchar(150) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `issuetype`
---
-
 CREATE TABLE `issuetype` (
   `id` int(11) NOT NULL,
   `name` varchar(45) NOT NULL,
   `icon` varchar(45) NOT NULL,
   `color` varchar(15) NOT NULL,
-  `showOnTop` boolean NOT NULL DEFAULT FALSE
+  `showOnTop` tinyint(1) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `relationships`
---
 
 CREATE TABLE `relationships` (
   `parent` int(11) NOT NULL,
@@ -103,105 +62,57 @@ CREATE TABLE `relationships` (
   `asOfDate` date NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `synonym`
---
-
 CREATE TABLE `synonym` (
   `entityid` int(11) NOT NULL,
   `synonym` varchar(150) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Indexes for dumped tables
---
 
---
--- Indexes for table `category`
---
 ALTER TABLE `category`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `id_UNIQUE` (`id`);
 
---
--- Indexes for table `entity`
---
+ALTER TABLE `categoryrelationships`
+  ADD KEY `parent` (`parent`),
+  ADD KEY `child` (`child`);
+
 ALTER TABLE `entity`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `id_UNIQUE` (`id`);
+ALTER TABLE `entity` ADD FULLTEXT KEY `name` (`name`);
 
---
--- Indexes for table `feedback`
---
 ALTER TABLE `feedback`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `id_UNIQUE` (`id`);
 
---
--- Indexes for table `issues`
---
 ALTER TABLE `issues`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `id_UNIQUE` (`id`);
 
---
--- Indexes for table `issuetype`
---
 ALTER TABLE `issuetype`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `id_UNIQUE` (`id`);
 
---
--- AUTO_INCREMENT for dumped tables
---
+ALTER TABLE `relationships`
+  ADD KEY `parent` (`parent`),
+  ADD KEY `child` (`child`);
 
---
--- AUTO_INCREMENT for table `category`
---
+ALTER TABLE `synonym`
+  ADD KEY `entityid` (`entityid`);
+
+
 ALTER TABLE `category`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT for table `entity`
---
 ALTER TABLE `entity`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT for table `feedback`
---
 ALTER TABLE `feedback`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT for table `issues`
---
 ALTER TABLE `issues`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT for table `issuetype`
---
 ALTER TABLE `issuetype`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
-
-
--- Current Database: `beeaccount`
-CREATE DATABASE /*!32312 IF NOT EXISTS*/ `beeaccount` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `beeaccount`;
-
--- Table structure for table `users`
-DROP TABLE IF EXISTS `users`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `users` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `username` varchar(45) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `rolebits` int DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id_UNIQUE` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
