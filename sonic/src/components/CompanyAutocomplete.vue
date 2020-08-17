3d65defce07e00d7c5734b6a3eb6b8a026bcfd7d
Haunted Bees <fench@hauntedbees.com>
2020-08-16 19:10:05 -0700

    searchbox enhancement

--- sonic/src/components/CompanyAutocomplete.vue
+++ sonic/src/components/CompanyAutocomplete.vue
@@ -7,13 +7,25 @@
         :items="items"
         :search-input.sync="search"
         :filter="Filter"
+        item-text="name"
+        item-value="name"
         class="mx-4"
         flat
         hide-no-data
         hide-details
         dark
         clearable
-        label="Search for a company or brand." />
+        label="Search for a company or brand.">
+        <template v-slot:item="{item}">
+            <v-list-item-content v-if="item.notFound === true">
+                <v-list-item-title v-text="`No results found for '${item.name}'`" />
+            </v-list-item-content>
+            <v-list-item-content v-if="item.notFound !== true">
+                <v-list-item-title v-text="item.name" />
+                 <v-list-item-subtitle v-if="item.parent !== null" v-text="'Owned by ' + item.parent" />
+            </v-list-item-content>
+        </template>
+    </v-autocomplete>
 </template>
 <script>
     import { bee } from "src/utils/webmethod.js";
@@ -28,18 +40,22 @@
         }),
         watch: {
             search(val) { val && val !== this.select && this.Query(val.trim()); },
-            select(val) { val && this.$emit("select", val.trim()); }
+            select(val) { val && this.$emit("select", val); }
         },
         methods: {
             Focus() { this.$refs.autocomplete.focus(); },
-            Filter(/*item, query, itemText*/) {
-                // TODO: make me matter?
-                //console.log(item, query, itemText);
-                return true;
-            },
+            Filter() { return true; },
             Query(query) {
                 if(query === "") { return; }
-                bee.get("SearchCompanies", query, data => { this.items = data.result.map(e => e.name); });
+                bee.get("SearchCompanies", query, data => {
+                    this.items = data.result;
+                    if(this.items.length === 0) {
+                        this.items = [{
+                            name: query,
+                            notFound: true
+                        }]
+                    }
+                });
             }
         }
     }
